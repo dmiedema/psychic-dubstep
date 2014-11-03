8c4f3437def31542bf11cd7390969284c0574753
dmiedema <daniel@househappy.org>
2014-11-02 17:18:27 -0800

    Initial Commit

---
+++ GM Taplist/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GM Taplist
+//
+//  Created by Daniel Miedema on 11/2/14.
+//  Copyright (c) 2014 Growl Movement. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ GM Taplist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GM Taplist
+//
+//  Created by Daniel Miedema on 11/2/14.
+//  Copyright (c) 2014 Growl Movement. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
